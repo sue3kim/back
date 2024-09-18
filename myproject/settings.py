b57e5641bc753e181afc9d0a5710fecf98ac4fa4
Suhyeon Kim <162249386+sue3kim@users.noreply.github.com>
2024-09-18 15:05:44 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -5,8 +5,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
-
 ALLOWED_HOSTS = [config('43.201.255.0')]
 
 
